c6d42d7e2308e1e06e27b5b48a0963e0fea94e40
Wyller Machado <wyller22@gmail.com>
2025-09-08 00:32:46 -0300

    VERSION == 1.2

--- version.py
+++ version.py
@@ -4,5 +4,5 @@ Application metadata constants.
 
 APP_NAME: str = "NFCopy"
 APP_DESC: str = "Leitor UID para NFC/RFID/CCID/PC/SC"
-VERSION: str = "1.1.0"
+VERSION: str = "1.2"
 AUTHOR: str = "Thoth Inovacoes LTDA"
